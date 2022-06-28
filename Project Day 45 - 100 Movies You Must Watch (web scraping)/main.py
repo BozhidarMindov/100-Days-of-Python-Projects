80c6dd7ab3da95d6944b174feb215c3a5399099f
Bozhidar Mindov <bbm210@aubg.edu>
2022-06-28 13:19:41 +0300

    making some changes

--- Project Day 45 - 100 Movies You Must Watch (web scraping)/main.py
+++ Project Day 45 - 100 Movies You Must Watch (web scraping)/main.py
@@ -9,19 +9,19 @@ ym_web_page = response.text
 
 #scraping the needed data
 soup = BeautifulSoup(ym_web_page, "html.parser")
-articles = soup.find_all(name="h3", class_="title")
+movies = soup.find_all(name="h3", class_="title")
 
 #storing the data in a list
-article_texts = []
-for article in articles:
-    article_texts.append(article.getText())
+movies_texts = []
+for movie in movies:
+    movies_texts.append(movies.getText())
 
 #reversing the list
-article_texts.reverse()
+movies_texts.reverse()
 
 #writing the list to a text file
 with open("movies.txt", "w", encoding="utf-8") as file:
     file.write("TOP 100 MOVIES TO WATCH\n\n")
-    for text in article_texts:
+    for text in movies_texts:
         file.write(f"{text}\n")
 
