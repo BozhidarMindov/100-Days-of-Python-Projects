064fca8a4830fbeb6055ae7845da9bded946cc92
Bozhidar Mindov <bbm210@aubg.edu>
2022-06-29 16:05:13 +0300

    Uploading the project from day 48

---
+++ Project Day 48 - Selenium Bot/main.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+
+cookie_url = "http://orteil.dashnet.org/experiments/cookie/"
+service = Service("G:/chromedriver_win32/chromedriver")
+driver = webdriver.Chrome(service=service)
+driver.get(cookie_url)
+
+#finding the cookie
+cookie = driver.find_element(By.ID, "cookie")
+
+#initializing the timing variables
+time_to_check = time.time() + 5    #5 seconds
+five_minutes = time.time() + 5*60  #5 minutes
+
+time_end = False
+
+while time_end == False:
+    #clicking on the cookie
+    cookie.click()
+
+    #checking the time
+    if time.time() > time_to_check:
+        can_buy = []
+        #finding the upgrade elemets
+        upgrades = driver.find_elements(By.CSS_SELECTOR, "#store div")
+        #buying the most expensive element we can afford
+        for upgrade in reversed(upgrades):
+            if upgrade.get_attribute("class") != "grayed":
+                can_buy.append(upgrade)
+        can_buy[0].click()
+        #adding another 5 seconds
+        time_to_check = time.time() + 5
+
+    #if the time gets over five minutes, we end the loop
+    if time.time() >= five_minutes:
+        cookie_per_second = driver.find_element(By.ID, value="cps").text
+        print(cookie_per_second)
+        time_end = True
+
+driver.quit()
